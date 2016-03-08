6b734caad6f52951bf9b252473add4bbad661e8d
Gerben <gerben@treora.com>
2016-03-08 11:39:22 +0100

    Near-complete rewrite.

--- polyfill.js
+++ polyfill.js
@@ -1,97 +1,52 @@
+// Polyfill to Element.scrollIntoView()
+
 (function () {
 
-function scrollIntoView (options) {
-  var window = this.ownerDocument.defaultView;
+// Original implementation is used for backwards compatibility
+var scrollIntoView_original = Element.prototype.scrollIntoView;
 
-  // Traditional scrollIntoView.
-  if (options === undefined || options === true
-                            || options === false) {
-    this.scrollIntoView(options);
+function scrollIntoView(options) {
+
+  // Use traditional scrollIntoView when traditional argument is given.
+  if (options === undefined || options === true || options === false) {
+    scrollIntoView_original.apply(this, arguments);
     return;
   }
 
-  // Fetch positional information.
-  //
-  // The following are always from the {top, bottom, left, right}
-  // of the viewport, to the {top, …} of the box.
-  // Think of them as geometrical vectors, it helps.
-  // The axes are directed downwards and towards the right.
-
-  var rect = this.getBoundingClientRect(),
-      topToBottom = rect.bottom,
-      bottomToTop = rect.top - window.innerHeight,
-      leftToRight = rect.right,
-      rightToLeft = rect.left - window.innerWidth,
-      xAllowed = true,  // We allow one translation on the x axis,
-      yAllowed = true;  // and one on the y axis.
+  var window = this.ownerDocument.defaultView;
 
   // Read options.
-  //
-  // We have the following options:
-  // - float vertical = 0.5        (from 0 to 1).
-  // - float horizontal = 0.0      (from 0 to 1).
-  // - boolean notIfViewed = true
-
-  if (options.vertical === undefined)  options.vertical = 0.5;
-  if (options.horizontal === undefined)  options.horizontal = 0.5;
-  if (options.evenIfViewed === undefined)  options.evenIfViewed = false;
-
-  // Whatever `horizontal` and `vertical` are,
-  // the behavior is the same if the box is (even partially) visible.
-
-  if (topToBottom > 0 && topToBottom <= this.offsetHeight) {
-    if (yAllowed) {
-      window.scrollBy(0, topToBottom - this.offsetHeight);
-      yAllowed = false;
-    }
-  } else
-  if (bottomToTop < 0 && bottomToTop >= -this.offsetHeight) {
-    if (yAllowed) {
-      window.scrollBy(0, bottomToTop + this.offsetHeight);
-      yAllowed = false;
-    }
+  if (options === undefined)  options = {};
+  if (options.center === true) {
+    options.vertical = 0.5;
+    options.horizontal = 0.5;
+  }
+  else {
+    if (options.block === "start")  options.vertical = 0.0;
+    else if (options.block === "end")  options.vertical = 0.0;
+    else if (options.vertical === undefined)  options.vertical = 0.0;
+    if (options.horizontal === undefined)  options.horizontal = 0.0;
   }
 
-  if (leftToRight > 0 && leftToRight <= this.offsetWidth) {
-    if (xAllowed) {
-      window.scrollBy(leftToRight - this.offsetWidth, 0);
-      xAllowed = false;
-    }
-  } else
-  if (rightToLeft < 0 && rightToLeft >= -this.offsetWidth) {
-    if (xAllowed) {
-      window.scrollBy(rightToLeft + this.offsetWidth, 0);
-      xAllowed = false;
-    }
-  }
+  // Fetch positional information.
+  var rect = this.getBoundingClientRect();
 
-  // If we want it positioned in the viewport,
-  // and the box is completely hidden,
-  // then we position it explicitly.
+  // Determine location to scroll to.
+  var targetY = window.scrollY + rect.top - (window.innerHeight - this.offsetHeight) * options.vertical;
+  var targetX = window.scrollX + rect.left - (window.innerWidth - this.offsetWidth) * options.horizontal;
 
-  if (yAllowed && (options.evenIfViewed? true:
-      (topToBottom <= 0 || bottomToTop >= 0))) {
-    window.scroll(window.scrollX,
-           window.scrollY + rect.top
-           - (window.innerHeight - this.offsetHeight) * options.vertical);
-  }
+  // Scroll.
+  window.scroll(targetX, targetY);
 
-  if (xAllowed && (options.evenIfViewed? true:
-      (leftToRight <= 0 || rightToLeft <= 0))) {
-    window.scroll(window.scrollX + rect.left
-           - (window.innerWidth - this.offsetWidth) * options.horizontal,
-           window.scrollY);
-  }
-
+  // If window is inside a frame, center that frame in the parent window. Recursively.
   if (window.parent !== window) {
     // We are inside a scrollable element.
-
     var frame = window.frameElement;
     scrollIntoView.call(frame, options);
   }
 }
 
 // Hook the polyfill.
-Element.prototype._scrollIntoView = scrollIntoView;
+Element.prototype.scrollIntoView = scrollIntoView;
 
 })();
